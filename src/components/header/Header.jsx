5a295b8d962342fcfb580747e5a2b23d798c6842
Stephen Wong <yummywongttv@gmail.com>
2022-04-25 19:18:26 -0400

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react"
 import './header.css'
+import CTA from  './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Stephen Wong</h1>
+                <h5 classname="text-light">Fullstack Developer</h5>
+                <CTA />
+                <HeaderSocial />
+
+                <div className="me">
+                    <img src={ME} alt="me" />
+                </div>
+                <a href="#contact" className="scroll_down">Scroll Down</a>
+            </div>
+        </header>
     )
 }
 
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+const HeaderSocial = () => {
+    return (
+        <div className='header__social'>
+            <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
+            <a href='https://github.com' target="_blank"></a>
+            <a href='https://dribbble.com' target="_blank"></a>
+        </div>
+    )
+}
+
+export default HeaderSocial
