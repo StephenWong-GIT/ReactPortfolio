fb259fc1bc1c0f44651058517ab967cfe14d6497
Stephen Wong <yummywongttv@gmail.com>
2022-04-25 18:16:06 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Testimonials />
+            <Contact />
+            <Footer />
+        </>
     )
 }
 
