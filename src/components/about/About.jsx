192065eb67d946a0bb8a83ea53f7302c0a827003
Stephen Wong <yummywongttv@gmail.com>
2022-04-25 21:33:15 -0400

    changed assets

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from "react"
 import './about.css'
+import ME from '../../assets/me-about.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
     return (
-        <section id='about'>About</section>
+        <section id='about'>
+            <h5>Get to Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="About Image" />
+                    </div>
+                </div>
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <FaAward className="about__icon"/>
+                            <h5>Experience</h5>
+                            <small>1+ Years Working</small>
+                        </article>
+
+                        <article className="about__card">
+                            <FiUsers className="about__icon"/>
+                            <h5>Clients</h5>
+                            <small>10+ Worldwide</small>
+                        </article>
+
+                        <article className="about__card">
+                            <VscFolderLibrary className="about__icon"/>
+                            <h5>Projects</h5>
+                            <small>30+ Completed</small>
+                        </article>
+
+                        <p>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        </p>
+
+                        <a href="#contact" className="btn btn-primary">Let's Talk!</a>
+                    </div>
+                <div className="about__content">
+
+                </div>
+            </div>
+        </section>
     )
 }
 
