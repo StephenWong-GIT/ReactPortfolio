bea5da3805b9ec090983ca6faee778c0bbcd690e
Stephen Wong <yummywongttv@gmail.com>
2022-04-26 13:11:45 -0400

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import './about.css'
-import ME from '../../assets/me-about.png'
+import ME from '../../assets/me-about.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
@@ -21,7 +21,7 @@ const About = () => {
                         <article className="about__card">
                             <FaAward className="about__icon"/>
                             <h5>Experience</h5>
-                            <small>1+ Years Working</small>
+                            <small>1.5+ Years Working</small>
                         </article>
 
                         <article className="about__card">
@@ -37,7 +37,7 @@ const About = () => {
                         </article>
 
                         <p>
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
                         </p>
 
                         <a href="#contact" className="btn btn-primary">Let's Talk!</a>
