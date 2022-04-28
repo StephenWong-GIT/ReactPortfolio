bb92997b1d3553e0caa92eb3a0a7f9271cbb90b7
Stephen Wong <yummywongttv@gmail.com>
2022-04-28 15:49:53 -0400

    added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,28 +4,28 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
-// const data = [
-//     {
-//         avatar: Avt1,
-//         name: '',
-//         reveiw: '',
-//     },
-//     {
-//         avatar: Avt2,
-//         name: '',
-//         reveiw: '',
-//     },
-//     {
-//         avatar: Avt3,
-//         name: '',
-//         reveiw: '',
-//     },
-//     {
-//         avatar: Avt4,
-//         name: '',
-//         reveiw: '',
-//     },
-// ]
+const data = [
+    {
+        avatar: AVTR1,
+        name: 'Francine Foo',
+        reveiw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus.',
+    },
+    {
+        avatar: AVTR2,
+        name: 'Seth Cotter',
+        reveiw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus.',
+    },
+    {
+        avatar: AVTR3,
+        name: 'Jason Lam',
+        reveiw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus.',
+    },
+    {
+        avatar: AVTR4,
+        name: 'James Depalo',
+        reveiw: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus.',
+    },
+]
 
 const Testimonials = () => {
     return (
@@ -42,44 +42,6 @@ const Testimonials = () => {
                         <small className="client__review">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. 
                             Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus. 
-                            Donec ac ante ac lorem venenatis convallis vitae sit amet lorem. Nam scelerisque euismod mi, id aliquam tortor. 
-                            Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                        </small>
-                </article>
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR2} alt="Avatar One" />
-                    </div>
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. 
-                            Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus. 
-                            Donec ac ante ac lorem venenatis convallis vitae sit amet lorem. Nam scelerisque euismod mi, id aliquam tortor. 
-                            Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                        </small>
-                </article>
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR3} alt="Avatar One" />
-                    </div>
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. 
-                            Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus. 
-                            Donec ac ante ac lorem venenatis convallis vitae sit amet lorem. Nam scelerisque euismod mi, id aliquam tortor. 
-                            Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                        </small>
-                </article>
-                <article className="testimonial">
-                    <div className="client__avatar">
-                        <img src={AVTR4} alt="Avatar One" />
-                    </div>
-                        <h5 className="client__name">Ernest Achiever</h5>
-                        <small className="client__review">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie aliquet pellentesque. Quisque faucibus in elit eu interdum. 
-                            Phasellus a elit ac orci pellentesque rhoncus ut a leo. Vestibulum ante nibh, mattis vitae tempus nec, pharetra at risus. 
-                            Donec ac ante ac lorem venenatis convallis vitae sit amet lorem. Nam scelerisque euismod mi, id aliquam tortor. 
-                            Interdum et malesuada fames ac ante ipsum primis in faucibus.
                         </small>
                 </article>
             </div>
