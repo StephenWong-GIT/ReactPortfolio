3f545aeda8d038121e46dc80ea585eec45138ce4
Stephen Wong <yummywongttv@gmail.com>
2022-05-01 19:37:46 -0400

    implimented contact me section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,9 +2,21 @@ import React from "react"
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react';
+import emailjs from 'emailjs-com'
 
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_plp040v', 'template_fdc2hzn', form.current, 'PmSWp5_ONIMgWrs9m')
+
+        e.target.reset()
+      };
+
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -31,7 +43,7 @@ const Contact = () => {
                     </article>
                 </div>
                 {/* END OF CONTACT OPTIONS */}
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name="name" placeholder="Your Full Name" required />
                     <input type="email" name="email" placeholder="Your Email" required />
                     <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
