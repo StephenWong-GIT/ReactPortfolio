0846e7c079af703357650793940cb64729521321
Stephen Wong <yummywongttv@gmail.com>
2022-05-02 01:18:34 -0400

    updated content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.png'
 import IMG4 from '../../assets/portfolio4.png'
