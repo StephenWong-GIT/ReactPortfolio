b8cfe5aa1e5af13edaee427a9b8d45f2211f57ce
Stephen Wong <yummywongttv@gmail.com>
2022-05-07 15:11:35 -0400

    updated content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,9 +20,7 @@ const Footer = () => {
             </ul>
 
             <div className="footer__socials">
-                <a href="https://facebook.com"><FaFacebookF/></a>
                 <a href="https://instagram.com"><FiInstagram/></a>
-                <a href="https://twitter.com"><IoLogoTwitter/></a>
             </div>
 
             <div className="footer__copyright">
